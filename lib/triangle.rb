3828bb06fe3eb13a4fda2f63b805f5c42c589c32
Allen F Partlow <allenfp@gmail.com>
2019-03-27 03:09:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :side_1, :side_2, :side_3, :type
 
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
@@ -8,9 +8,10 @@ class Triangle
     @side_3 = side_3
   end
 
+
   def kind
     if side_1 == side_2 && side_2 == side_3
-
+      @type = :equilateral
     end
 
   end
