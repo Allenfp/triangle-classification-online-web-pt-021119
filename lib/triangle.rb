e09c9f981c0100cf8b6673a2226b8f68753480b2
Allen F Partlow <allenfp@gmail.com>
2019-03-27 03:46:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,13 +11,22 @@ class Triangle
 
   def kind
     if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 
-
+      raise TriangleError
     elsif side_1 == side_2 && side_2 == side_3
       @type = :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       @type = :isosceles
     else
       @type = :scalene
+    end
+
+  end
+
+  class Triangle
+  # triangle code
+   
+    class TriangleError < StandardError
+      # triangle error code
     end
 
   end
